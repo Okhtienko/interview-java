52089d51a588492c126d78246f2aaafb624f54d0
Okhtienko <mariohtienko@gmail.com>
2023-06-01 17:05:13 +0300

    modified files to the majority element problem

--- array-string/src/majority_element/Main.java
+++ array-string/src/majority_element/Main.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class Main {
 	public static void main(String[] args) {
-		int[] numbers = new int[]{2, 2, 1, 1, 1, 2, 2};
+		final int[] numbers = new int[]{2, 2, 1, 1, 1, 2, 2};
 		System.out.println(majorityElement(numbers));
 	}
 
